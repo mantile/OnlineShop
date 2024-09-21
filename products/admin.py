68e6f73c3f706bb4eee6f472a7deaec8b3b41262
Mantile <ggwp1337@yandex.ru>
2024-09-21 20:12:32 +0300

    README and ignore

--- products/admin.py
+++ products/admin.py
@@ -4,5 +4,5 @@ from .models import Product
 #Models import and registration
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price') #name and price to display
+    list_display = ('name', 'price', 'quantity') #name and price to display
     search_fields = ('name',)
